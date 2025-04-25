1256ba20ee5def9064cb8e1b452113de72296e2b
Deadman621 <affan.basit116@gmail.com>
2025-04-25 21:02:20 +0500

    Minor Changes #5
    Minor Changes #5
    
    New branch for testing

--- pipeline/chunking/src/file_tracker.c
+++ pipeline/chunking/src/file_tracker.c
@@ -6,11 +6,10 @@
 
 processed_file_t *processed_files = NULL; 
 
-// Function to add a filename to the hash table
 void add_processed_file(const char *filename) {
     processed_file_t *entry = malloc(sizeof(processed_file_t));
     if (!entry) {
-        perror("add_processed_file: Failed to allocate memory for hash entry");
+        perror("add_processed_file - Failed to allocate memory for hash entry");
         return; 
     }
 
@@ -19,14 +18,12 @@ void add_processed_file(const char *filename) {
     HASH_ADD_STR(processed_files, name, entry);
 }
 
-// Function to check if a filename is in the hash table
 bool was_file_processed(const char *filename) {
     processed_file_t *entry;
     HASH_FIND_STR(processed_files, filename, entry);
     return entry != NULL;
 }
 
-// Function to free the hash table (call this at cleanup)
 void free_processed_files(void) {
     processed_file_t *current_entry, *tmp;
     HASH_ITER(hh, processed_files, current_entry, tmp) {
--- pipeline/chunking/src/image_chunker.c
+++ pipeline/chunking/src/image_chunker.c
@@ -18,11 +18,10 @@ extern chunk_queue_t chunker_filtering_queue;
 unsigned char *load_image(const char *filename, int *width, int *height, int *channels) {
     unsigned char *data = stbi_load(filename, width, height, channels, 0);
     if (data == NULL) {
-        fprintf(stderr, "Error loading image '%s': %s\n", filename, stbi_failure_reason());
+        fprintf(stderr, "load_image: Error loading image '%s': %s\n", filename, stbi_failure_reason());
         return NULL;
     }
 
-    printf("Loaded image '%s' (%d x %d, %d channels)\n", filename, *width, *height, *channels);
     return data;
 }
 
@@ -48,7 +47,7 @@ static int create_chunks_internal(const char *original_filename,
 /*     image_chunk_t **chunks = (image_chunk_t **)malloc(*num_chunks_total * sizeof(image_chunk_t *));
     if (chunks == NULL) {
         perror("create_chunks_internal: Failed to allocate memory for chunk array");
-        return NULL; // Should return -1 to match function signature
+        return NULL; 
     } */
 
     int current_chunk_index = 0;
@@ -68,7 +67,7 @@ static int create_chunks_internal(const char *original_filename,
             if (chunk == NULL) {
                 perror("create_chunks_internal: Failed to allocate memory for chunk struct");
                 exit_status = -1; 
-                goto cleanup_loop; 
+                goto cleanup_image; 
             }
 
             chunk->original_image_name = NULL;
@@ -95,10 +94,9 @@ static int create_chunks_internal(const char *original_filename,
             chunk->original_image_name = strdup(original_filename);
             if (chunk->original_image_name == NULL) {
                 perror("create_chunks_internal: Failed to allocate memory for chunk filename (strdup)");
-                free_image_chunk(chunk); // Safe to call even if pixel_data is NULL
-                // chunk = NULL; // Not necessary, chunk is freed
+                free_image_chunk(chunk);
                 exit_status = -1;
-                goto cleanup_loop;
+                goto cleanup_image;
             }
 
             chunk->data_size_bytes = chunk->width * chunk->height * bytes_per_pixel;
@@ -107,7 +105,7 @@ static int create_chunks_internal(const char *original_filename,
                 perror("create_chunks_internal: Failed to allocate memory for chunk pixel data");
                 free_image_chunk(chunk);
                 exit_status = -1;
-                goto cleanup_loop;
+                goto cleanup_image;
             }
 
             /*
@@ -120,18 +118,18 @@ static int create_chunks_internal(const char *original_filename,
                 The sequence of bytes is stored as follows: [P(0, 0)-R, P(0, 0)-G, P(0, 0)-B, P(0, 1)-R, P(0, 1)-G, P(0, 1)-B]
             */
 
-            size_t src_row_stride = width * bytes_per_pixel; // bytes per row in the image
+            size_t src_bytes_per_row = width * bytes_per_pixel; // bytes per row in the image
             size_t chunk_row_bytes = chunk->width * bytes_per_pixel; // Bytes to copy per row for this chunk
 
             for(size_t row = 0; row < chunk->height; row++) {
 
                 /*
                     Writing the bytes row by row, because data for a single chunk is not contigously stored.
-                    `(chunk->offset_y + row) * src_row_stride` is the number of bytes to skip from the start of the image.
+                    `(chunk->offset_y + row) * src_bytes_per_row` is the number of bytes to skip from the start of the image.
                     `chunk->offset_x * bytes_per_pixel` is the number of bytes to skip from the start of the row
                 */
 
-                unsigned char *src_ptr = image_data + (chunk->offset_y + row) * src_row_stride + chunk->offset_x * bytes_per_pixel;
+                unsigned char *src_ptr = image_data + (chunk->offset_y + row) * src_bytes_per_row + chunk->offset_x * bytes_per_pixel;
 
                 /*
                     The destination pointer only needs to calculate the pointer offset for the current row; Since,
@@ -153,26 +151,22 @@ static int create_chunks_internal(const char *original_filename,
                         pthread_self(), current_chunk_index, original_filename);
                 free_image_chunk(chunk); 
                 exit_status = -1;
-                goto cleanup_loop; 
+                goto cleanup_image; 
             } else {
-                // Successfully enqueued, ownership transferred to queue.
-                // Do NOT free 'chunk' here.
+
             }
 
             current_chunk_index++;
         }
     }
 
-    cleanup_loop: 
-        if (stop_flag) {
-            printf("Thread %lu: Stop flag detected during chunk creation for %s.\n", pthread_self(), original_filename);
-            exit_status = -1; 
-        }
+    cleanup_image: 
+        if (stop_flag) exit_status = -1; 
 
         if (exit_status == 0) 
-            printf("Thread %lu: Finished creating %d chunks for %s.\n", pthread_self(), current_chunk_index, original_filename);
+            /* printf("Thread %lu: Finished creating %d chunks for %s.\n", pthread_self(), current_chunk_index, original_filename) */;
         else {
-            fprintf(stderr, "Thread %lu: Failed or stopped during chunk creation for %s (processed %d chunks).\n", pthread_self(), original_filename, current_chunk_index);
+            //fprintf(stderr, "Thread %lu: Failed or stopped during chunk creation for %s (processed %d chunks).\n", pthread_self(), original_filename, current_chunk_index);
             discarded_images_table_add(original_filename);
         }
 
@@ -204,9 +198,6 @@ void *chunk_image_thread(void *arg) {
         int calc_chunk_width = (width < fixed_chunk_width)? width: fixed_chunk_width;
         int calc_chunk_height = (height < fixed_chunk_height)? height: fixed_chunk_height;
 
-        if (width > 0 && calc_chunk_width <= 0) calc_chunk_width = 1;
-        if (height > 0 && calc_chunk_height <= 0) calc_chunk_height = 1;
-
         printf("Chunker thread %lu: Processing %s with target chunk size: %dx%d\n",
             pthread_self(), filename, calc_chunk_width, calc_chunk_height);
 
@@ -219,7 +210,6 @@ void *chunk_image_thread(void *arg) {
 
         if (output != 0) 
             fprintf(stderr, "Chunker thread failed for %s.\n", filename);
-            // need handling;
 
         stbi_image_free(image_data);
         image_data = NULL;
@@ -228,7 +218,6 @@ void *chunk_image_thread(void *arg) {
     }
 
     printf("Chunker thread finished successfully.\n");
-    
     
     return NULL;
 }
